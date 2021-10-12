d7f62c6d86d1c0c9f1b6b828b10bf4bb3f661cde
Sohee <sohee3480@gmail.com>
2021-10-12 21:27:44 +0900

    application 등록 Koin 시작하기

---
+++ app/src/main/java/com/example/aoppart5chapter01/AopPart5Chapter01Application.kt
@@ -0,0 +1,24 @@
+package com.example.aoppart5chapter01
+
+import android.app.Application
+import com.example.aoppart5chapter01.di.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+
+class AopPart5Chapter01Application: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+//        TODO Koin Trigger
+
+        startKoin{
+            androidLogger(Level.ERROR)
+            androidContext(this@AopPart5Chapter01Application)
+            modules(appModule)
+
+        }
+    }
+
+}
