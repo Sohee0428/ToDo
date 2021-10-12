4738e3d515e7516e4fa009e99ebe7849336fae1e
Sohee <sohee3480@gmail.com>
2021-10-12 21:31:45 +0900

    데이터 클래스 만들기  - Entity

---
+++ app/src/main/java/com/example/aoppart5chapter01/data/entity/ToDoEntity.kt
@@ -0,0 +1,8 @@
+package com.example.aoppart5chapter01.data.entity
+
+data class ToDoEntity(
+    val id: Long = 0,
+    val title: String,
+    val description: String,
+    val hasCompleted: Boolean = false
+)
