f81908ff7bc8b23f4914cca71c8d46b3234dae20
Sohee <sohee3480@gmail.com>
2021-10-13 22:25:55 +0900

    데이터 베이스 만들기

--- app/src/main/java/com/example/aoppart5chapter01/data/repository/ToDoRepository.kt
+++ app/src/main/java/com/example/aoppart5chapter01/data/repository/ToDoRepository.kt
@@ -12,12 +12,16 @@ interface ToDoRepository {
 
     suspend fun getToDoList(): List<ToDoEntity>
 
+    suspend fun insertToDoItem(toDoItem: ToDoEntity): Long
+
     suspend fun insertToDoList(toDoList: List<ToDoEntity>)
 
-    suspend fun updateToDoItem(toDoItem: ToDoEntity): Boolean
+    suspend fun updateToDoItem(toDoItem: ToDoEntity)
 
     suspend fun getToDoItem(itemId: Long): ToDoEntity?
 
     suspend fun deleteAll()
+
+    suspend fun deleteToDoItem(id: Long)
 
 }
